6aa8fe007e545916776cd88b848f712dc339e1e2
Bondy_And_ray <bondy@and-ray.ru>
2026-02-20 16:16:44 +0300

    v 1.0.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,25 @@
 import Vacancies from "./pages/Vacancies";
 import { Header } from "./components/Header";
 import { DescriptionVacancy } from "./pages/DescriptionVacancy";
-import {
-  HashRouter,
-  Navigate,
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import { ErrorPage } from "./pages/ErrorPage";
 
 function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
+  return (
+    <>
       <HashRouter>
-        <Route>
+        <Header />
+        <Routes>
           <Route
             path="/"
             element={<Navigate to="/vacancies/moscow" replace />}
           />
-          <Route path="/vacancies">
-            <Route path="moscow" element={<Vacancies />} />
-            <Route path="petersburg" element={<Vacancies />} />
-            <Route path=":id" element={<DescriptionVacancy />} />
-            <Route path="*" element={<ErrorPage />} />
-          </Route>
-        </Route>
-      </HashRouter>,
-    ),
-  );
-  return (
-    <>
-      <Header />
-      <RouterProvider router={router} />
+          <Route path="/vacancies/moscow" element={<Vacancies />} />
+          <Route path="/vacancies/petersburg" element={<Vacancies />} />
+          <Route path="/vacancies/:id" element={<DescriptionVacancy />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </HashRouter>
     </>
   );
 }
